29d800f3390123bfe19e95823ee2b41b0712bb17
Noah <55927481+Noah-Flame@users.noreply.github.com>
2025-11-11 23:31:32 +0800

    Update run_eval.sh

--- mini_interact/knowledge_based/mini_interact_conv/evaluation/run_eval.sh
+++ mini_interact/knowledge_based/mini_interact_conv/evaluation/run_eval.sh
@@ -1,4 +1,4 @@
-python "/Path/to/livesql_sqlite/evaluation/wrapper_evaluation_sqlite.py" \
-    --jsonl_file "/Path/to/livesql_sqlite/new_data/data_30.jsonl" \
-    --db_path "/Path/to/livesql_sqlite/database" \
-    --mode "gold" \ # or pred
+python "/Path/to/bird-interact-sqlite/evaluation/wrapper_evaluation_sqlite.py" \
+    --jsonl_file "/Path/to/bird-interact-sqlite/new_data/data_30.jsonl" \
+    --db_path "/Path/to/bird-interact-sqlite/database" \
+    --mode "pred" \ # or gold
